1c64ebf25777b5ee8596551a15c4cf7655fa2188
Imbojeong <qhwjd8447@naver.com>
2023-12-04 16:52:20 +0900

    보정 - npy들 딕셔러니 생성

---
+++ src/main/python/category_dict.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+'''
+array_ani= np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Animation.npy')
+
+# 초기화된 딕셔너리 생성
+ani_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_ani)):
+key = f'{i+1}'  # 'anime_숫자' 형태의 키 생성
+value = array_ani[i]  # 원하는 값 (예: array_anime)을 할당
+ani_dict[key] = value  # 딕셔너리에 추가
+
+print(ani_dict)
+
+
+array_y2k = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Y2K.npy')
+
+# 초기화된 딕셔너리 생성
+y2k_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_y2k)):
+key = f'{i+1}'  # 'anime_숫자' 형태의 키 생성
+value = array_y2k[i]  # 원하는 값 (예: array_anime)을 할당
+y2k_dict[key] = value  # 딕셔너리에 추가
+
+print(y2k_dict)
+
+
+array_game = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Game.npy')
+
+
+# 초기화된 딕셔너리 생성
+game_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_game)):
+key = f'{i}'  # 'anime_숫자' 형태의 키 생성
+value = array_game[i]  # 원하는 값 (예: array_anime)을 할당
+game_dict[key] = value  # 딕셔너리에 추가
+
+print(game_dict)
+
+array_art = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Art.npy')
+
+# 초기화된 딕셔너리 생성
+art_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_art)):
+key = f'{i}'  # 'anime_숫자' 형태의 키 생성
+value = array_art[i]  # 원하는 값 (예: array_anime)을 할당
+art_dict[key] = value  # 딕셔너리에 추가
+
+print(art_dict)
+
+
+array_fashion = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Fashion.npy')
+
+# 초기화된 딕셔너리 생성
+fashion_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_fashion)):
+key = f'{i+1}'  # 'anime_숫자' 형태의 키 생성
+value = array_fashion[i]  # 원하는 값 (예: array_anime)을 할당
+fashion_dict[key] = value  # 딕셔너리에 추가
+
+print(fashion_dict)
+
+array_modern = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Modern.npy')
+
+# 초기화된 딕셔너리 생성
+modern_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_modern)):
+key = f'{i+1}'  # 'anime_숫자' 형태의 키 생성
+value = array_modern[i]  # 원하는 값 (예: array_anime)을 할당
+modern_dict[key] = value  # 딕셔너리에 추가
+
+print(modern_dict)
+
+array_graphic = np.load('C:\\Users\\82105\\OneDrive\\바탕 화면\\Case\\src\\main\\python\\src\\main\\python\\data\\Graphic.npy')
+
+# 초기화된 딕셔너리 생성
+graphic_dict = {}
+
+# 원하는 횟수만큼 반복하여 딕셔너리에 데이터 추가
+for i in range(0, len(array_graphic)):
+key = f'{i+1}'  # 'anime_숫자' 형태의 키 생성
+value = array_graphic[i]  # 원하는 값 (예: array_anime)을 할당
+graphic_dict[key] = value  # 딕셔너리에 추가
+
+'''
+
